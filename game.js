d3bfbc5722146d34c82810f162c79a27aaa372f0
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2021-11-02 16:47:19 -0400

    Convert tabs to spaces

--- game.js
+++ game.js
@@ -2,59 +2,59 @@ const $ = (x) => document.querySelector(x);
 const $$ = (x) => document.querySelectorAll(x);
 
 const Player = (name, teamSymbol) => {
-	let score = 0;
-	const getName = () => name;
-	const getTeamSymbol = () => teamSymbol;
-	const getScore = () => score;
-	const incrementScore = () => { 
-		score++;	
-	}
-	const play = (x, y, gameBoard) => {
-		if (gameBoard.getCell(x, y) === "") {
-			gameBoard.setCell(x, y, teamSymbol)	
-			gameBoard.updateCellDisplay(x, y, teamSymbol);
-      gameBoard.nextTurn();
-		} else {
-			console.log(`This cell is already taken. Choose another.`);
-		}
-	};
+    let score = 0;
+    const getName = () => name;
+    const getTeamSymbol = () => teamSymbol;
+    const getScore = () => score;
+    const incrementScore = () => { 
+        score++;
+    }
+    const play = (x, y, gameBoard) => {
+        if (gameBoard.getCell(x, y) === "") {
+            gameBoard.setCell(x, y, teamSymbol)
+            gameBoard.updateCellDisplay(x, y, teamSymbol);
+            gameBoard.nextTurn();
+        } else {
+            console.log(`This cell is already taken. Choose another.`);
+        }
+    };
 
-  return {getName, getTeamSymbol, getScore, incrementScore, play}
+    return {getName, getTeamSymbol, getScore, incrementScore, play}
 }
 
 const gameBoard = (() => {
-	'use strict';
-	const xMax = 3;
-	const yMax = 3;
-  let playerTurn = true; // first player's turn
-	let board = new Array(xMax).fill().map(() => Array(yMax).fill(""));
-	const getCell = (x, y) => {
-		if (x < xMax && y < yMax) {
-			return board[x][y];
-		}
-	}
-  const getTurn = () => { return playerTurn };
-  const nextTurn = () => { playerTurn = !playerTurn };
-	const setCell = (x, y, value) => {
-		if (x < xMax && y < yMax && typeof(value) === "string") {
-			board[x][y] = value;
-		} else {
-			console.log(`Invalid cell value: cannot set cell ${x},${y} to ${value}`);
-		}
-	};
+    'use strict';
+    const xMax = 3;
+    const yMax = 3;
+    let playerTurn = true; // first player's turn
+    let board = new Array(xMax).fill().map(() => Array(yMax).fill(""));
+    const getCell = (x, y) => {
+        if (x < xMax && y < yMax) {
+            return board[x][y];
+        }
+    }
+    const getTurn = () => { return playerTurn };
+    const nextTurn = () => { playerTurn = !playerTurn };
+    const setCell = (x, y, value) => {
+        if (x < xMax && y < yMax && typeof(value) === "string") {
+            board[x][y] = value;
+        } else {
+            console.log(`Invalid cell value: cannot set cell ${x},${y} to ${value}`);
+        }
+    };
 
-	const updateCellDisplay = (x, y, value) => {
-		let cell = $(`#board tr[data-row="${x}"] td[data-col="${y}"]`);
-		cell.textContent = value;
-	};
-	
-	const eraseCell = (x, y) => {
-		if (x < xMax && y < yMax) {
-			board[x][y] = "";
-		}
-	};
+    const updateCellDisplay = (x, y, value) => {
+        let cell = $(`#board tr[data-row="${x}"] td[data-col="${y}"]`);
+        cell.textContent = value;
+    };
+        
+    const eraseCell = (x, y) => {
+        if (x < xMax && y < yMax) {
+                board[x][y] = "";
+        }
+    };
 
-	return {board, getTurn, nextTurn, getCell, setCell, updateCellDisplay, eraseCell};
+    return {board, getTurn, nextTurn, getCell, setCell, updateCellDisplay, eraseCell};
 })();
 
 // Game State and Events
